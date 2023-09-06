6814c9fccf1bad318b71a0287d7ee77007271aaa
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2023-09-05 23:02:51 -0500

    Add banner welcome message at start

--- camel.py
+++ camel.py
@@ -8,10 +8,17 @@ def camelcase(sentence):
     # So this still works on empty strings, strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """ Display program name """
+    message = "Awesome camelCase Program"
+    stars = "*" * len(message)
+    print(f'\n{stars} \n{message} \n{stars} \n')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
-    
+
 if __name__ == '__main__':
     main()
